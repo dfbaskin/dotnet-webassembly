3454324e8afca21fe75e14b073fc165245be24be
Dave Baskin <dfbaskin@gmail.com>
2023-04-08 18:11:53 -0400

    Updating test lib with QR coder.

---
+++ TestLib/QR.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using QRCoder;
+
+namespace TestLib;
+
+public partial class QR
+{
+    [JSExport]
+    public static string Generate(string text, int pixelsPerBlock)
+    {
+        var qrGenerator = new QRCodeGenerator();
+        var qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+
+        var qrCode = new BitmapByteQRCode(qrCodeData);
+        return Convert.ToBase64String(qrCode.GetGraphic(pixelsPerBlock));
+    }
+}
