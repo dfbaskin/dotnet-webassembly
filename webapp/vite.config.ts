72a54b478ea3f5b551ee07a4fb34a7825a6adf7b
Dave Baskin <dfbaskin@gmail.com>
2023-04-08 17:00:24 -0400

    Adding dotnet integration.

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 4200,
+    proxy: {
+      "/dn": {
+        target: "http://localhost:4201",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/dn/, ""),
+      },
+    },
+  },
+});
