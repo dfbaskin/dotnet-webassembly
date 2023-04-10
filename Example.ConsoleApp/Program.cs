946487c38a4b55ad577fde5d1f7d25f65520d91f
Dave Baskin <dfbaskin@gmail.com>
2023-04-10 10:58:29 -0400

    Adding .NET console example.

---
+++ Example.ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using Example.RustComponent;
+
+Console.WriteLine($"Running on {Environment.OSVersion}");
+var testVec = new Vec2 { x = 4, y = 10 };
+Console.WriteLine($"Original Vector ({testVec.x},{testVec.y})");
+var resultVec = RustComponentInterop.double_vector(testVec);
+Console.WriteLine($"Doubled Vector ({resultVec.x},{resultVec.y})");
